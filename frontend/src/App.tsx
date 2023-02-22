d5dda010c88cec128dbaab539b6211807a6c8e13
Arseniy <artemkazagorodin2040@gmail.com>
2023-02-22 22:57:20 +0600

    gay

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import './App.css';
+import Marketplace from './components/Marketplace/Marketplace';
 
 function App() {
   return (
     <>
+    <Marketplace />
     </>
   );
 }
