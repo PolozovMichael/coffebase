ec639347c8130814ee9035b8316283d3050a1ecb
Arseniy <artemkazagorodin2040@gmail.com>
2023-02-22 23:33:10 +0600

    miha gay

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
 import './App.scss'
+import Marketplace from './components/Marketplace/Marketplace'
+import Profilebar from './components/Profilebar/Profilebar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Marketplace />
+    <Profilebar />
+    </>
   )
 }
 
