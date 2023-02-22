2beaccebed5be8daa4f3ba68c61faa98cd11f925
PolozovMichael <polozovmisha.sm.nis.edu.kz@gmail.com>
2023-02-22 22:25:39 +0600

    first commit

---
+++ backend/routes/clientRouter.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const {
+    createClient,
+    getListOfClients,
+    getClientById,
+    updateClientById,
+    removeClient,
+    changeClientOrder
+} = require('../controllers/clientController');
+
+router.post('/api/createClient/:orderId', createClient);
+router.get('/api/getListOfClients', getListOfClients);
+router.get('/api/:clientId', getClientById);
+router.put('/api/updateClient/:clientId', updateClientById);
+router.put('/api/changeClientOrder/:clientId/:orderId', changeClientOrder);
+router.delete('/api/deleteClient/:clientId', removeClient);
+
+module.exports = router;
